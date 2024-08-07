e2957ecf6b838c944f9d5367104c9381581c51d5
Frame <33612694+flame123flame@users.noreply.github.com>
2024-08-07 14:01:45 +0700

    add

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,25 +6,24 @@ import { NoAuthGuard } from './core/guard/no-auth.guard';
 export const routes: Routes = [
     { path: '', pathMatch: 'full', redirectTo: '/auth' },
     {
-      path: "auth",
-      loadChildren: () => import("./modules/authentication/authentication.routes"),
-      canActivate: [NoAuthGuard],
+        path: "auth",
+        loadChildren: () => import("./modules/authentication/authentication.routes"),
+        canActivate: [NoAuthGuard],
     },
     {
-        path: "home",
+        path: "dashboard",
         loadChildren: () => import("./modules/dashboard/dashboard.routes"),
         canActivate: [NoAuthGuard],
-      },
-    {
-      path: '403',
-      canActivate: [NoAuthGuard],
-      loadComponent: () => import('./core/common/403/403.component').then(c => c.Exception403Component)
     },
     {
-      path: '404',
-      canActivate: [NoAuthGuard],
-      loadComponent: () => import('./core/common/404/404.component').then(c => c.Exception404Component)
+        path: '403',
+        canActivate: [NoAuthGuard],
+        loadComponent: () => import('./core/common/403/403.component').then(c => c.Exception403Component)
+    },
+    {
+        path: '404',
+        canActivate: [NoAuthGuard],
+        loadComponent: () => import('./core/common/404/404.component').then(c => c.Exception404Component)
     },
     { path: '**', component: RedirectComponent }
-  ];
-  
+];
