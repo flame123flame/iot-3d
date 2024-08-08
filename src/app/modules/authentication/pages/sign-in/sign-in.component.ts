4eb33e4d499105f48971b5b9e7bf6743e9e6346b
Frame <33612694+flame123flame@users.noreply.github.com>
2024-08-08 10:44:01 +0700

    add

--- src/app/modules/authentication/pages/sign-in/sign-in.component.ts
+++ src/app/modules/authentication/pages/sign-in/sign-in.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { EngineComponent } from '../../../../shared/components/engine/engine.component';
 import { NgZorroAntdModule } from '../../../../shared/ng-zorro-antd.module';
 import { SharedModule } from '../../../../shared/shared.module';
+import { ThreejsViewerComponent } from '../../../../shared/components/threejs-viewer/threejs-viewer.component';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [EngineComponent,NgZorroAntdModule,SharedModule],
+  imports: [EngineComponent,ThreejsViewerComponent,NgZorroAntdModule,SharedModule],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.scss'
 })
